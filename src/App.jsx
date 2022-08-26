f1dac7a20270c7c8c889daba325417594934ee30
lava <lavakumared15@gmail.com>
2022-08-26 16:20:48 +0530

    added posthog

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
   );
 
   useEffect(() => {
-    posthog.init(POSTHOG_KEY, { api_host: 'https://app.posthog.com' })
+    posthog.init(process.env.POSTHOG_KEY, { api_host: 'https://app.posthog.com' })
     Set_query_params(window.location.search);
     document.documentElement.style.setProperty(
       "--border",
