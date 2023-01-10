b572e4a033fe44680bb73368c1f5d52b4e98f76f
Lava <lavakumared15@gmail.com>
2023-01-10 20:30:40 +0530

    feature-update

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,4 +1,4 @@
-const { default: axios } = require("axios");
+const axios = require("axios");
 
 const getProductId = (stateObj, assessment_type) => {
   // const hair_current_condition = window.localStorage.getItem(
